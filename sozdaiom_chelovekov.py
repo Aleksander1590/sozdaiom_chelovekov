18d018ea1b0ac9a33131b224a485e2ef8010a0cc
Aleksander1590 <romanov1590@gmail.com>
2024-11-29 12:30:13 +0100

    Update sozdaiom_chelovekov.py

--- sozdaiom_chelovekov.py
+++ sozdaiom_chelovekov.py
@@ -4,58 +4,59 @@ import random
 import os
 from runic_alphabet import runic_alphabet
 
-fake = Faker("ru_RU")
-first_name = fake.first_name()
-last_name = fake.last_name()
-town = fake.city()
-job = fake.job()
-strength = random.randint(3, 18)
-agility = random.randint(3, 18)
-endurance = random.randint(3, 18)
-intelligence = random.randint(3, 18)
-luck = random.randint(3, 18)
+def main():
+    fake = Faker("ru_RU")
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    town = fake.city()
+    job = fake.job()
+    strength = random.randint(3, 18)
+    agility = random.randint(3, 18)
+    endurance = random.randint(3, 18)
+    intelligence = random.randint(3, 18)
+    luck = random.randint(3, 18)
 
-skills = [
-    "Стремительный прыжок", 
-    "Электрический выстрел", 
-    "Ледяной удар", 
-    "Стремительный удар", 
-    "Кислотный взгляд", 
-    "Тайный побег", 
-    "Ледяной выстрел", 
-    "Огненный заряд"
-]
+    skills = [
+        "Стремительный прыжок", 
+        "Электрический выстрел", 
+        "Ледяной удар", 
+        "Стремительный удар", 
+        "Кислотный взгляд", 
+        "Тайный побег", 
+        "Ледяной выстрел", 
+        "Огненный заряд"
+    ]
 
-for i in range(1, 11):
+    for i in range(1, 11):
 
-    card_skills = random.sample(skills, 3)
-    skill_1 = card_skills[0]
-    skill_2 = card_skills[1]
-    skill_3 = card_skills[2]
+        card_skills = random.sample(skills, 3)
+        skill_1 = card_skills[0]
+        skill_2 = card_skills[1]
+        skill_3 = card_skills[2]
 
-    for key, value in runic_alphabet.items():
-        skill_1 = skill_1.replace(key, value)
-        skill_2 = skill_2.replace(key, value)
-        skill_3 = skill_3.replace(key, value)
+        for key, value in runic_alphabet.items():
+            skill_1 = skill_1.replace(key, value)
+            skill_2 = skill_2.replace(key, value)
+            skill_3 = skill_3.replace(key, value)
 
-    context = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "town": town,
-        "job": job,
-        "strength": strength,
-        "agility": agility,
-        "endurance": endurance,
-        "intelligence": intelligence,
-        "luck": luck,
-        "skill_1": skill_1,
-        "skill_2": skill_2,
-        "skill_3": skill_3
-    }
+        context = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "town": town,
+            "job": job,
+            "strength": strength,
+            "agility": agility,
+            "endurance": endurance,
+            "intelligence": intelligence,
+            "luck": luck,
+            "skill_1": skill_1,
+            "skill_2": skill_2,
+            "skill_3": skill_3
+        }
 
-    def main():    
         os.makedirs("files", exist_ok=True)
         file_operations.render_template("template.svg", f'files/card-{i}.svg', context)
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
+
